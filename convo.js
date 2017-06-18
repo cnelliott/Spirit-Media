95767b8727618821f33ca742c717d8f15277a8b1
Arnold Flash Hundert <hunderta@yahoo.com>
2017-06-18 18:05:58 -0400

    added location

---
+++ convo.js
@@ -0,0 +1,35 @@
+module.exports = (text, event, id) => {
+	if (text.includes('foo')) {
+		return {
+			text: 'FOOBAR'
+		}
+	} // if cat
+
+	if (text.includes('list')) {
+		return {
+			"attachment": {
+				"type": "template",
+				"payload": {
+					"template_type": "button",
+					"text": "What do you want to do next?",
+					"buttons": [{
+							"type": "web_url",
+							"url": "https://petersapparel.parseapp.com",
+							"title": "Show Website"
+						}, ] // buttons
+				} // payload
+			} // attachment
+		};
+	} // if list
+
+	if (text.includes('location')) {
+		return {
+				"text": "Please share your location:",
+				"quick_replies": [{
+					"content_type": "location",
+				}]
+		} //location
+	}
+
+
+}
